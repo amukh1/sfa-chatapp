8f9a0a3bd83a769d1c4d66541bb2f6f133248b3d
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-10-31 03:43:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+
+
+document.getElementById('body').classList.add('blur');
+
+if(window.localStorage.getItem('blur') == 'on') {
+    document.getElementById('body').classList.add('blur');
+    }else if(window.localStorage.getItem('blur') == 'off') {
+    document.getElementById('body').classList.add('noblur');
+    document.getElementById('body').classList.remove('blur');
+    document.getElementById('body').classList.remove('noblur');
+    window.localStorage.setItem('blur', 'off')
+    }
+
+    function httpSend(msg) {
+      // document.getElementById('body').classList.add('blur');
+
+      const elements = document.querySelectorAll('#chat');
+      let finalElement = elements.length - 1
+      
+      if(elements[finalElement].innerHTML == msg){
+        return
+      }
+      let div = document.createElement('div');
+  div.classList.add('chat');
+  div.setAttribute("id", "chat");
+  // div.setAttribute("onClick", "fullScreen()")
+  document.getElementById('chatContainer').appendChild(div)
+  div.innerHTML = `${msg}`
+
+  var count= $('div').filter('.chat').length;
+  console.log(count)
+  if(count >= 6) {
+      $('.chatContainer').find('div').first().remove();
+  }
+  
+  if(count >= 7) {
+      console.log('oh no!')
+  }
+
+  }
+
+function send() {
+  
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         // Typical action to be performed when the document is ready:
+         console.log(xhr.responseText)
+         if(xhr.responseText !== '0412154527'){
+          //  console.log(lastHTTP)
+         httpSend(xhr.responseText)
+         }else {
+           console.log('null')
+         }
+      }
+  };
+  xhr.open("post", `https://chatapi.amukh1.repl.co/apiPOST?msg=${document.getElementById('input').value}`, true);
+  xhr.send();
+
+    // document.getElementById('body').classList.add('blur');
+    
+    let div = document.createElement('div');
+div.classList.add('chat');
+div.setAttribute("id", "chat");
+// div.setAttribute("onClick", "fullScreen()")
+document.getElementById('chatContainer').appendChild(div)
+div.innerHTML = `${document.getElementById('input').value}`
+var count= $('div').filter('.chat').length;
+console.log(count)
+if(count >= 6) {
+    $('.chatContainer').find('div').first().remove();
+}
+
+if(count >= 7) {
+    console.log('oh no!')
+}
+}
+
+hotkeys('c,b,r,/,enter', function (event, handler){
+    switch (handler.key) {
+      case 'c': {
+        document.getElementById('body').classList.add('noblur');
+        document.getElementById('body').classList.remove('blur');
+        document.getElementById('body').classList.remove('noblur');
+        window.localStorage.setItem('blur', 'off')
+      }
+        break;
+      case 'b': {
+        document.getElementById('body').classList.add('blur');
+        window.localStorage.setItem('blur', 'on')
+      }
+        break;
+      case 'r': alert('you pressed r!');
+        break;
+      case '/': {
+        document.getElementById('input').focus()
+      }
+        break;
+        case 'enter': {
+            send()
+        }
+        break;
+      default: alert(event);
+    }
+  });
+
+
+  
+
+  function update() {
+    var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       console.log(xhr.responseText)
+       if(xhr.responseText !== '0412154527'){
+        //  console.log(lastHTTP)
+       httpSend(xhr.responseText)
+       }else {
+         console.log('null')
+       }
+    }
+};
+xhr.open("GET", "https://chatapi.amukh1.repl.co/api", true);
+xhr.send();
+  }
